514116da6395b4b897b823be0ad7294a98b54328
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-08 16:44:28 +0200

    lldp: read LLDP data from systemd
    This reads the LLDP data provided by systemd.
    
    Change-Id: I9b6ce8b766ecc908bebc316a1e44610d9403ee88

--- src/LLDP.h
+++ src/LLDP.h
@@ -10,6 +10,7 @@
 #include <map>
 #include <spdlog/fmt/ostr.h> // allow spdlog to use operator<<(ostream, NeighbourEntry)
 #include <string>
+#include <systemd/sd-lldp.h>
 #include <vector>
 
 namespace lldp::lldp {
@@ -25,6 +26,11 @@ class LLDPDataProvider {
 public:
     virtual ~LLDPDataProvider();
     virtual std::vector<NeighbourEntry> getNeighbours() const;
+
+private:
+    static std::vector<std::pair<int, std::string>> networkdListLinks();
+    static bool open_lldp_neighbors(int ifindex, FILE** ret);
+    static int next_lldp_neighbor(FILE* f, sd_lldp_neighbor** ret);
 };
 
 } /* namespace lldp::lldp */
