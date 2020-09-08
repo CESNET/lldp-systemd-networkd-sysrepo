78b92fb88e59f99c82aad629621f1f667a23618c
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-08 16:44:28 +0200

    lldp: read LLDP data from systemd
    Modify LLDPDataProvider so it can read LLDP data from systemd's files
    using systemd's library functions.
    
    Reading the data from systemd also requires listing the links via
    networkd's dbus interface. Therefore (for testing purposes) we create
    a DBus server that mimics the behaviour of networkd bus manager.
    
    Change-Id: I9b6ce8b766ecc908bebc316a1e44610d9403ee88

--- src/LLDP.cpp
+++ src/LLDP.cpp
@@ -4,15 +4,106 @@
  * Written by Tomáš Pecka <tomas.pecka@fit.cvut.cz>
  *
  */
+#include <netinet/ether.h>
+#include <spdlog/spdlog.h>
 #include "LLDP.h"
 
 namespace lldp::lldp {
 
+void sd_lldp_neighbor_deleter::operator()(sd_lldp_neighbor* e) const {
+    sd_lldp_neighbor_unrefp(&e);
+}
+
+LLDPDataProvider::LLDPDataProvider(std::filesystem::path dataDirectory, sdbus::IConnection& dbusConnection, const std::string& dbusNetworkdBus, const std::string& dbusNetworkdObject, const std::string& dbusNetworkdInterface)
+    : m_dataDirectory(std::move(dataDirectory))
+    , m_networkdDbusProxy(sdbus::createProxy(dbusConnection, dbusNetworkdBus, dbusNetworkdObject))
+    , m_networkdDbusInterface(dbusNetworkdInterface)
+{
+}
+
+/* @brief Lists links using networkd dbus interface and returns them as a list of pairs <link_id, link_name>. */
+std::vector<std::pair<int, std::string>> LLDPDataProvider::listLinks() const
+{
+    std::vector<sdbus::Struct<int, std::string, sdbus::ObjectPath>> links;
+    std::vector<std::pair<int, std::string>> res; // we only want to return pairs (linkId, linkName), we do not need dbus object path
+
+    m_networkdDbusProxy->callMethod("ListLinks").onInterface(m_networkdDbusInterface).storeResultsTo(links);
+
+    std::transform(links.begin(), links.end(), std::back_inserter(res), [](const auto& e) { return std::make_pair(std::get<0>(e), std::get<1>(e)); });
+    return res;
+}
+
+/* @brief Reads a LLDP neighbour entry from systemd's binary LLDP files.
+ *
+ * Inspired systemd's networkctl code.
+ */
+std::unique_ptr<sd_lldp_neighbor, sd_lldp_neighbor_deleter> LLDPDataProvider::nextNeighbor(std::ifstream& ifs)
+{
+    size_t rawSz; // get neighbour size in bytes
+
+    // read neighbor size; also (by systemd): each LLDP packet is at most MTU size, but let's allow up to 4KiB just in case
+    ifs.read(reinterpret_cast<char*>(&rawSz), 1 * sizeof(rawSz));
+    if (size_t rd = ifs.gcount(); (rd == 0 && ifs.eof()) || rd != sizeof(rawSz) || le64toh(rawSz) >= 4096) {
+        return nullptr;
+    }
+
+    std::vector<uint8_t> raw;
+    raw.reserve(le64toh(rawSz));
+
+    ifs.read(reinterpret_cast<char*>(raw.data()), 1 * le64toh(rawSz));
+    if (static_cast<size_t>(ifs.gcount()) != le64toh(rawSz)) { // typecast is safe here (see std::streamsize)
+        return nullptr;
+    }
+
+    // let systemd parse from raw
+    sd_lldp_neighbor* tmp = nullptr;
+    if (sd_lldp_neighbor_from_raw(&tmp, raw.data(), le64toh(rawSz)) < 0) {
+        return nullptr;
+    }
+    return sd_lldp_neighbor_managed(tmp);
+}
+
 std::vector<NeighborEntry> LLDPDataProvider::getNeighbors() const
 {
-    return {
-        NeighborEntry{"dummy1", {{"remotePortId", "dummy2"}}},
-    };
+    std::vector<NeighborEntry> res;
+
+    for (const auto& [linkId, linkName] : listLinks()) {
+        spdlog::debug("LLDP: Collecting neighbours on '{}' (id {})", linkName, linkId);
+
+        // open lldp datafile
+        std::filesystem::path lldpFilename = m_dataDirectory / std::to_string(linkId);
+        std::ifstream ifs(lldpFilename, std::ios::binary);
+
+        if (!ifs.is_open()) {
+            spdlog::debug("  failed to open LLDP file ({}). It is probable that LLDP is not enabled for the link or missing privileges to open the file.", lldpFilename);
+            continue;
+        }
+
+        for (sd_lldp_neighbor_managed n = nextNeighbor(ifs); n != nullptr; n = nextNeighbor(ifs)) {
+            NeighborEntry ne;
+            ne.m_portId = linkName;
+
+            if (const char* system_name = nullptr; sd_lldp_neighbor_get_system_name(n.get(), &system_name) >= 0) {
+                ne.m_properties["remoteSysName"] = system_name;
+            }
+            if (const char* port_id = nullptr; sd_lldp_neighbor_get_port_id_as_string(n.get(), &port_id) >= 0) {
+                ne.m_properties["remotePortId"] = port_id;
+            }
+            if (const char* chassis_id = nullptr; sd_lldp_neighbor_get_chassis_id_as_string(n.get(), &chassis_id) >= 0) {
+                ne.m_properties["remoteChassisId"] = chassis_id;
+            }
+            if (ether_addr* addr = nullptr; sd_lldp_neighbor_get_destination_address(n.get(), addr) >= 0) {
+                ne.m_properties["remoteMgmtAddress"] = ether_ntoa(addr);
+            }
+
+            // TODO: Implement enabled and supported capabilities
+
+            spdlog::trace("  found neighbor {}", ne);
+            res.push_back(ne);
+        }
+    }
+
+    return res;
 }
 
 std::ostream& operator<<(std::ostream& os, const NeighborEntry& entry)
--- tests/sysrepo-opsdata.cpp
+++ tests/sysrepo-opsdata.cpp
@@ -1,22 +1,41 @@
 #include <sysrepo-cpp/Connection.hpp>
-#include "callback.h"
 #include "LLDP.h"
-
+#include "callback.h"
+#include "configure.cmake.h"
+#include "dbus-helpers/dbus_server.h"
 #include "doctest_integration.h"
 #include "test_log_setup.h"
 #include "test_sysrepo_helpers.h"
 
-TEST_CASE("Connection")
+/* Tests are correctly announced by sysrepo */
+TEST_CASE("Sysrepo opsdata callback")
 {
     TEST_INIT_LOGS;
     TEST_INIT_SYSREPO;
 
-    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>();
+    // Use the real paths but prefixed with "local." or "/local"
+    const std::string bus = "local.org.freedesktop.network1",
+                      object = "/local/org/freedesktop/network1",
+                      interface = "local.org.freedesktop.network1.Manager";
+
+    auto dbusServerConnection = sdbus::createSessionBusConnection(bus);
+    auto dbusClientConnection = sdbus::createSessionBusConnection();
+    dbusServerConnection->enterEventLoopAsync();
+
+    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>(CMAKE_CURRENT_SOURCE_DIR "/tests/files/lldp1", *dbusClientConnection, bus, object, interface);
     srSubs->dp_get_items_subscribe("/czechlight-lldp:nbr-list", std::make_shared<lldp::sysrepo::Callback>(lldp));
 
+    auto dbusServer = DbusServer(*dbusServerConnection, object, interface);
+    dbusServer.setLinks({{8, "ve-image"}});
+
     REQUIRE(dataFromSysrepo(clSess, "/czechlight-lldp:nbr-list") == std::map<std::string, std::string> {
-                {"/if-name[ifName='dummy1']", ""},
-                {"/if-name[ifName='dummy1']/ifName", "dummy1"},
-                {"/if-name[ifName='dummy1']/remotePortId", "dummy2"},
+                {"/if-name[ifName='ve-image']", ""},
+                {"/if-name[ifName='ve-image']/ifName", "ve-image"},
+                {"/if-name[ifName='ve-image']/remotePortId", "host0"},
+                {"/if-name[ifName='ve-image']/remoteSysName", "image"},
+                {"/if-name[ifName='ve-image']/remoteChassisId", "7062a9e41c924ac6942da39c56e6b820"},
+
             });
+
+    dbusServerConnection->leaveEventLoop();
 }
