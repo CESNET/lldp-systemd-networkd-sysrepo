644932c8e3c6dd89adde3297b453039735b5173b
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-08 16:44:28 +0200

    lldp: read LLDP data from systemd
    Modify LLDPDataProvider so it can read LLDP data from systemd's files
    using systemd's library functions.
    
    Reading the data from systemd also requires listing the links via
    networkd's dbus interface. Therefore (for testing purposes) we create
    a DBus server that mimics the behaviour of networkd bus manager.
    
    Change-Id: I9b6ce8b766ecc908bebc316a1e44610d9403ee88

---
+++ tests/lldp-systemd.cpp
@@ -0,0 +1,61 @@
+#include <sdbus-c++/sdbus-c++.h>
+#include <unistd.h>
+#include "LLDP.h"
+#include "configure.cmake.h"
+#include "dbus-helpers/dbus_server.h"
+#include "doctest_integration.h"
+#include "test_log_setup.h"
+
+using namespace std::literals;
+
+namespace lldp::lldp {
+bool operator==(const NeighborEntry& a, const NeighborEntry& b)
+{
+    return std::tie(a.m_portId, a.m_properties) == std::tie(b.m_portId, b.m_properties);
+}
+} /* namespace lldp::lldp */
+
+TEST_CASE("Parsing with the mock")
+{
+    TEST_INIT_LOGS;
+
+    const auto serverBus = "local.pid" + std::to_string(getpid()) + ".org.freedesktop.network1";
+    auto dbusServerConnection = sdbus::createSessionBusConnection(serverBus);
+    dbusServerConnection->enterEventLoopAsync();
+
+    std::vector<std::pair<int, std::string>> links;
+    std::string dataDir;
+    std::vector<lldp::lldp::NeighborEntry> expected;
+
+    SECTION("LLDP active on a single link")
+    {
+        links = {{1, "lo"}, {2, "enp0s25"}, {3, "wlp3s0"}, {4, "tun0"}, {5, "br-53662f640039"}, {6, "docker0"}, {7, "br-e78120c0adda"}, {8, "ve-image"}};
+        dataDir = "single-link";
+        expected = {{"ve-image", {{"remoteSysName", "image"}, {"remotePortId", "host0"}, {"remoteChassisId", "7062a9e41c924ac6942da39c56e6b820"}}}};
+    }
+
+    SECTION("No LLDP enabled")
+    {
+        links = {{1, "lo"}, {2, "enp0s25"}, {3, "wlp3s0"}, {4, "tun0"}, {5, "br-53662f640039"}, {6, "docker0"}, {7, "br-e78120c0adda"}, {8, "ve-image"}};
+        dataDir = "no-link";
+        expected = {};
+    }
+
+    auto dbusServer = DbusServer(*dbusServerConnection);
+    dbusServer.setLinks(links); // intentionally not mocking DbusMockServer::ListLinks but using explicit set/get pattern so I can avoid an unneccesary dependency on trompeloeil
+
+    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>(std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR "/tests/files/"s) / dataDir, std::move(sdbus::createSessionBusConnection()), serverBus);
+
+    REQUIRE(lldp->getNeighbors() == expected);
+}
+
+#if LIST_NEIGHBORS_RUN
+TEST_CASE("Real systemd")
+{
+    TEST_INIT_LOGS;
+
+    auto dbusConnection = sdbus::createSystemBusConnection();
+    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>("/run/systemd/netif/lldp", *dbusConnection, "org.freedesktop.network1");
+    [[maybe_unused]] auto x = lldp->getNeighbors();
+}
+#endif
--- tests/sysrepo-opsdata.cpp
+++ tests/sysrepo-opsdata.cpp
@@ -1,22 +1,35 @@
 #include <sysrepo-cpp/Connection.hpp>
-#include "callback.h"
+#include <unistd.h>
 #include "LLDP.h"
-
+#include "callback.h"
+#include "configure.cmake.h"
+#include "dbus-helpers/dbus_server.h"
 #include "doctest_integration.h"
 #include "test_log_setup.h"
 #include "test_sysrepo_helpers.h"
 
-TEST_CASE("Connection")
+/* Tests are correctly announced by sysrepo */
+TEST_CASE("Sysrepo opsdata callback")
 {
     TEST_INIT_LOGS;
     TEST_INIT_SYSREPO;
 
-    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>();
+    const auto serverBus = "local.pid" + std::to_string(getpid()) + ".org.freedesktop.network1";
+    auto dbusServerConnection = sdbus::createSessionBusConnection(serverBus);
+    dbusServerConnection->enterEventLoopAsync();
+
+    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>(CMAKE_CURRENT_SOURCE_DIR "/tests/files/single-link", std::move(sdbus::createSessionBusConnection()), serverBus);
     srSubs->dp_get_items_subscribe("/czechlight-lldp:nbr-list", std::make_shared<lldp::sysrepo::Callback>(lldp));
 
+    auto dbusServer = DbusServer(*dbusServerConnection);
+    dbusServer.setLinks({{8, "ve-image"}});
+
     REQUIRE(dataFromSysrepo(clSess, "/czechlight-lldp:nbr-list") == std::map<std::string, std::string> {
-                {"/if-name[ifName='dummy1']", ""},
-                {"/if-name[ifName='dummy1']/ifName", "dummy1"},
-                {"/if-name[ifName='dummy1']/remotePortId", "dummy2"},
+                {"/if-name[ifName='ve-image']", ""},
+                {"/if-name[ifName='ve-image']/ifName", "ve-image"},
+                {"/if-name[ifName='ve-image']/remotePortId", "host0"},
+                {"/if-name[ifName='ve-image']/remoteSysName", "image"},
+                {"/if-name[ifName='ve-image']/remoteChassisId", "7062a9e41c924ac6942da39c56e6b820"},
+
             });
 }
