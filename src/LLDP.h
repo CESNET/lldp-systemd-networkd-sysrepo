49843bbe3ef6c9eab9cc4630558f9065b17979d4
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-08 16:44:28 +0200

    lldp: read LLDP data from systemd
    Modify LLDPDataProvider so it can read LLDP data from systemd's files
    using systemd's library functions.
    
    Reading the data from systemd also requires listing the links via
    networkd's dbus interface. Therefore (for testing purposes) we create
    a DBus server that mimics the behaviour of networkd bus manager.
    
    Change-Id: I9b6ce8b766ecc908bebc316a1e44610d9403ee88

--- src/LLDP.h
+++ src/LLDP.h
@@ -7,9 +7,13 @@
 
 #pragma once
 
+#include <fstream>
+#include <filesystem>
 #include <map>
+#include <sdbus-c++/sdbus-c++.h>
 #include <spdlog/fmt/ostr.h> // allow spdlog to use operator<<(ostream, NeighborEntry)
 #include <string>
+#include <systemd/sd-lldp.h>
 #include <vector>
 
 namespace lldp::lldp {
@@ -20,10 +24,24 @@ struct NeighborEntry {
 };
 std::ostream& operator<<(std::ostream& os, const NeighborEntry& entry);
 
+/** @brief sd_lldp_neighbor requires deletion by invoking sd_lldp_neighbor_unrefp */
+struct sd_lldp_neighbor_deleter {
+    void operator()(sd_lldp_neighbor* e) const;
+};
+using sd_lldp_neighbor_managed = std::unique_ptr<sd_lldp_neighbor, sd_lldp_neighbor_deleter>;
 
 class LLDPDataProvider {
 public:
+    LLDPDataProvider(std::filesystem::path dataDirectory, sdbus::IConnection& dbusConnection, const std::string& dbusNetworkdBus, const std::string& dbusNetworkdObject, const std::string& dbusNetworkdInterface);
     std::vector<NeighborEntry> getNeighbors() const;
+
+private:
+    std::filesystem::path m_dataDirectory;
+    std::unique_ptr<sdbus::IProxy> m_networkdDbusProxy;
+    std::string m_networkdDbusInterface;
+
+    std::vector<std::pair<int, std::string>> listLinks() const;
+    static sd_lldp_neighbor_managed nextNeighbor(std::ifstream& ifs);
 };
 
 } /* namespace lldp::lldp */
---
+++ tests/lldp-systemd.cpp
@@ -0,0 +1,68 @@
+#include <sdbus-c++/sdbus-c++.h>
+#include "LLDP.h"
+#include "configure.cmake.h"
+#include "dbus-helpers/dbus_server.h"
+#include "doctest_integration.h"
+#include "test_log_setup.h"
+
+using namespace std::literals;
+
+namespace lldp::lldp {
+bool operator==(const NeighborEntry& a, const NeighborEntry& b)
+{
+    return std::tie(a.m_portId, a.m_properties) == std::tie(b.m_portId, b.m_properties);
+}
+} /* namespace lldp::lldp */
+
+TEST_CASE("Parsing with the mock")
+{
+    TEST_INIT_LOGS;
+
+    // Use the real paths but prefixed with "local." or "/local"
+    const std::string bus = "local.org.freedesktop.network1",
+                      object = "/local/org/freedesktop/network1",
+                      interface = "local.org.freedesktop.network1.Manager";
+
+    auto dbusServerConnection = sdbus::createSessionBusConnection(bus);
+    auto dbusClientConnection = sdbus::createSessionBusConnection();
+
+    std::vector<std::pair<int, std::string>> links;
+    std::string dataDir;
+    std::vector<lldp::lldp::NeighborEntry> expected;
+
+    SECTION("LLDP active on a single link")
+    {
+        links = {{1, "lo"}, {2, "enp0s25"}, {3, "wlp3s0"}, {4, "tun0"}, {5, "br-53662f640039"}, {6, "docker0"}, {7, "br-e78120c0adda"}, {8, "ve-image"}};
+        dataDir = "single-link";
+        expected = {{"ve-image", {{"remoteSysName", "image"}, {"remotePortId", "host0"}, {"remoteChassisId", "7062a9e41c924ac6942da39c56e6b820"}}}};
+    }
+
+    SECTION("No LLDP enabled")
+    {
+        links = {{1, "lo"}, {2, "enp0s25"}, {3, "wlp3s0"}, {4, "tun0"}, {5, "br-53662f640039"}, {6, "docker0"}, {7, "br-e78120c0adda"}, {8, "ve-image"}};
+        dataDir = "no-link";
+        expected = {};
+    }
+
+    auto dbusServer = DbusServer(*dbusServerConnection, object, interface);
+    dbusServer.setLinks(links); // intentionally not mocking DbusMockServer::ListLinks but using explicit set/get pattern so I can avoid an unneccesary dependency on trompeloeil
+    dbusServerConnection->enterEventLoopAsync();
+
+    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>(std::filesystem::path(CMAKE_CURRENT_SOURCE_DIR "/tests/files/"s) / dataDir, *dbusClientConnection, bus, object, interface);
+    auto neighbors = lldp->getNeighbors();
+
+    REQUIRE(neighbors == expected);
+
+    dbusServerConnection->leaveEventLoop();
+}
+
+#if LIST_NEIGHBORS_RUN
+TEST_CASE("Real systemd")
+{
+    TEST_INIT_LOGS;
+
+    auto dbusConnection = sdbus::createSystemBusConnection();
+    auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>("/run/systemd/netif/lldp", *dbusConnection, "org.freedesktop.network1", "/org/freedesktop/network1", "org.freedesktop.network1.Manager");
+    [[maybe_unused]] auto x = lldp->getNeighbors();
+}
+#endif
