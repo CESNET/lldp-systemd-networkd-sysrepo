9e641cff6d61ed56f949da4d8b1ec91f24a88dc5
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-08 16:44:28 +0200

    lldp: read LLDP data from systemd
    Modify LLDPDataProvider so it can read LLDP data from systemd's files
    using systemd's library functions.
    
    Reading the data from systemd also requires listing the links via
    networkd's dbus interface. Therefore (for testing purposes) we create
    a DBus server that mimics the behaviour of networkd bus manager.
    
    Change-Id: I9b6ce8b766ecc908bebc316a1e44610d9403ee88

--- src/LLDP.h
+++ src/LLDP.h
@@ -7,9 +7,13 @@
 
 #pragma once
 
+#include <filesystem>
+#include <fstream>
 #include <map>
+#include <sdbus-c++/sdbus-c++.h>
 #include <spdlog/fmt/ostr.h> // allow spdlog to use operator<<(ostream, NeighborEntry)
 #include <string>
+#include <systemd/sd-lldp.h>
 #include <vector>
 
 namespace lldp::lldp {
@@ -20,10 +24,17 @@ struct NeighborEntry {
 };
 std::ostream& operator<<(std::ostream& os, const NeighborEntry& entry);
 
-
 class LLDPDataProvider {
 public:
+    LLDPDataProvider(std::filesystem::path dataDirectory, sdbus::IConnection& dbusConnection, const std::string& dbusNetworkdBus, const std::string& dbusNetworkdObject, const std::string& dbusNetworkdInterface);
     std::vector<NeighborEntry> getNeighbors() const;
+
+private:
+    std::filesystem::path m_dataDirectory;
+    std::unique_ptr<sdbus::IProxy> m_networkdDbusProxy;
+    std::string m_networkdDbusInterface;
+
+    std::vector<std::pair<int, std::string>> listLinks() const;
 };
 
 } /* namespace lldp::lldp */
