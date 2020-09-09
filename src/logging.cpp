66d10fda2589b9d2b5201af39f363fa7f773fed5
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-09 15:02:09 +0200

    initialize codebase
    Add CMake configuration, main source file with basic logger and
    clang-format configuration file. Initialize CI.
    
    Introduce sysrepo operational data callback dummy and set up
    sysrepo logging. This is mainly a preparation for the next
    commits, where the real operational data will be filled in
    the callback.
    
    Most setup adapted from [1] (logging, test stucture, cmake setup, docs)
    and [2] (subscription).
    
    [1] cla-sysrepo@db9cd2761ec0c16dab01828993229c51c113d878
    [2] netconf-cli@54c34aeb8904e83b6d6c9fdc9f1abc0d98a5c208
    
    Change-Id: I87b070547f81faa1f63a0d502c7f3d4b8d740b0c

---
+++ src/logging.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2020 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>, Tomáš Pecka <tomas.pecka@fit.cvut.cz>
+ *
+ */
+
+extern "C" {
+#include <sysrepo.h>
+}
+#include "logging.h"
+
+extern "C" {
+
+/** @short Propagate sysrepo events to spdlog */
+static void spdlog_sr_log_cb(sr_log_level_t level, const char* message)
+{
+    // Thread safety note: this is, as far as I know, thread safe:
+    // - the static initialization itself is OK
+    // - all loggers which we instantiate are thread-safe
+    // - std::shared_ptr::operator-> is const, and all const members of that class are documented to be thread-safe
+    static auto log = spdlog::get("sysrepo");
+    assert(log);
+    switch (level) {
+    case SR_LL_NONE:
+    case SR_LL_ERR:
+        log->error(message);
+        break;
+    case SR_LL_WRN:
+        log->warn(message);
+        break;
+    case SR_LL_INF:
+        log->info(message);
+        break;
+    case SR_LL_DBG:
+        log->debug(message);
+        break;
+    }
+}
+}
+
+namespace lldp::utils {
+
+/** @short Initialize logging
+
+Creates and registers all required loggers and connect them to the provided sink.
+*/
+void initLogs(std::shared_ptr<spdlog::sinks::sink> sink)
+{
+    auto defaultLogger = std::make_shared<spdlog::logger>("lldp-systemd-sysrepo", sink);
+    spdlog::register_logger(defaultLogger);
+    spdlog::set_default_logger(defaultLogger);
+
+    spdlog::register_logger(std::make_shared<spdlog::logger>("sysrepo", sink));
+    sr_log_set_cb(spdlog_sr_log_cb);
+}
+
+} /* namespace lldp::utils */
---
+++ tests/pretty_printers.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016-2018 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+*/
+
+#pragma once
+
+#include <doctest/doctest.h>
+#include <map>
+#include <sstream>
+
+namespace doctest {
+
+template <>
+struct StringMaker<std::map<std::string, std::string>> {
+    static String convert(const std::map<std::string, std::string>& map)
+    {
+        std::ostringstream os;
+        os << "{" << std::endl;
+        for (const auto& [key, value] : map) {
+            os << "  \"" << key << "\": \"" << value << "\"," << std::endl;
+        }
+        os << "}";
+        return os.str().c_str();
+    }
+};
+
+}
