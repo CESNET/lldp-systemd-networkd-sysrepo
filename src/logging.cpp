61c8d229b6b2663e4beb248f7f037e0558627dc0
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-15 15:49:08 +0200

    add sysrepo ops data callback
    Introduces sysrepo operational data callback dummy and sets
    up sysrepo logging. This is mainly a preparation for the
    next commits, where the real operational data will be
    filled in the callback.
    
    Adapted from [1,2] (subscription) and [2] (logging).
    
    [1] netconf-cli@54c34aeb8904e83b6d6c9fdc9f1abc0d98a5c208
    [2] cla-sysrepo@db9cd2761ec0c16dab01828993229c51c113d878
    
    Change-Id: Ifb4f14be72a62109d375a6e9fbabbd768274d041

--- src/logging.cpp
+++ src/logging.cpp
@@ -8,15 +8,50 @@
 
 #include "logging.h"
 
+extern "C" {
+#include <sysrepo.h>
+}
+#include "logging.h"
+
+extern "C" {
+
+/** @short Propagate sysrepo events to spdlog */
+static void spdlog_sr_log_cb(sr_log_level_t level, const char* message)
+{
+    // Thread safety note: this is, as far as I know, thread safe:
+    // - the static initialization itself is OK
+    // - all loggers which we instantiate are thread-safe
+    // - std::shared_ptr::operator-> is const, and all const members of that class are documented to be thread-safe
+    static auto log = spdlog::get("sysrepo");
+    assert(log);
+    switch (level) {
+    case SR_LL_NONE:
+    case SR_LL_ERR:
+        log->error(message);
+        break;
+    case SR_LL_WRN:
+        log->warn(message);
+        break;
+    case SR_LL_INF:
+        log->info(message);
+        break;
+    case SR_LL_DBG:
+        log->debug(message);
+        break;
+    }
+}
+}
+
 namespace lldp::utils {
 
 /** @short Initialize logging
 
 Creates and registers all required loggers and connect them to the provided sink.
 */
-void initLogs([[maybe_unused]] std::shared_ptr<spdlog::sinks::sink> sink)
+void initLogs(std::shared_ptr<spdlog::sinks::sink> sink)
 {
-
+    spdlog::register_logger(std::make_shared<spdlog::logger>("sysrepo", sink));
+    sr_log_set_cb(spdlog_sr_log_cb);
 }
 
 } /* namespace lldp::utils */
