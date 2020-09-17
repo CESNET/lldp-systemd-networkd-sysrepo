00f9627fc0ed65fe89d926270e54b0a1043ea821
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-09-17 22:34:19 +0200

    lldp: Add LLDP data provider
    Introduce LLDPDataProvider class which handles retrieval of LLDP
    neighbours data.
    Modify sysrepo's daemon callback to call this class.
    
    Change-Id: I6b428ff86ce02d25cb014342a853fad7473a388a

--- tests/sysrepo-opsdata.cpp
+++ tests/sysrepo-opsdata.cpp
@@ -2,6 +2,7 @@
 #include "sysrepo/Daemon.h"
 
 #include "trompeloeil_doctest.h"
+#include "fake.h"
 #include "test_log_setup.h"
 #include "test_sysrepo_helpers.h"
 
@@ -10,11 +11,29 @@ TEST_CASE("Connection")
     TEST_SYSREPO_INIT_LOGS;
     trompeloeil::sequence seq1;
 
+    auto dataProvider = std::make_shared<FakeDataProvider>();
+
     auto srConn = std::make_shared<sysrepo::Connection>("lldp-systemd-networkd-sysrepo");
-    auto daemon = std::make_shared<lldp::sysrepo::Daemon>(srConn, "/czechlight-lldp:nbr-list", "czechlight-lldp", "2020-08-25");
+    auto daemon = std::make_shared<lldp::sysrepo::Daemon>(srConn, "/czechlight-lldp:nbr-list", "czechlight-lldp", "2020-08-25", dataProvider);
+
+    auto lst = {
+        lldp::lldp::NeighbourEntry {"eth0", {{"remotePortId", "rem0"}}},
+        lldp::lldp::NeighbourEntry {"eth1", {{"remotePortId", "rem0"}, {"remoteSysName", "n2"}}},
+    };
+    FAKE_NEIGHBOURS(lst);
 
     auto conn = std::make_shared<sysrepo::Connection>("test-client");
     auto session = std::make_shared<sysrepo::Session>(conn);
 
-    REQUIRE(true); // TODO
+
+    CHECK(dataFromSysrepo(session, "/czechlight-lldp:nbr-list") == std::map<std::string, std::string>({
+              {"/if-name[ifName='eth0']", ""},
+              {"/if-name[ifName='eth0']/ifName", "eth0"},
+              {"/if-name[ifName='eth0']/remotePortId", "rem0"},
+
+              {"/if-name[ifName='eth1']", ""},
+              {"/if-name[ifName='eth1']/ifName", "eth1"},
+              {"/if-name[ifName='eth1']/remotePortId", "rem0"},
+              {"/if-name[ifName='eth1']/remoteSysName", "n2"},
+          }));
 }
