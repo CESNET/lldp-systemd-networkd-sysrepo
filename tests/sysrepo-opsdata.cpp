137efa536e6c0c1693f9568600e4c9857f05df78
Tomáš Pecka <tomas.pecka@fit.cvut.cz>
2020-10-01 21:32:16 +0200

    test timeout
    Change-Id: I5c9cefa5bebbc6c316577b4997079fa44551df08

--- tests/sysrepo-opsdata.cpp
+++ tests/sysrepo-opsdata.cpp
@@ -19,11 +19,20 @@ TEST_CASE("Sysrepo opsdata callback")
     auto dbusClientConnection = sdbus::createSessionBusConnection();
     dbusServerConnection->enterEventLoopAsync();
 
+    spdlog::info("dbus-connection-eventloopasync");
+
     auto lldp = std::make_shared<lldp::lldp::LLDPDataProvider>(CMAKE_CURRENT_SOURCE_DIR "/tests/files/single-link", *dbusClientConnection, serverBus);
+
+    spdlog::info("lldp");
+
     srSubs->dp_get_items_subscribe("/czechlight-lldp:nbr-list", std::make_shared<lldp::sysrepo::Callback>(lldp));
+
+    spdlog::info("subscribed");
 
     auto dbusServer = DbusServer(*dbusServerConnection);
     dbusServer.setLinks({{8, "ve-image"}});
+
+    spdlog::info("server");
 
     REQUIRE(dataFromSysrepo(clSess, "/czechlight-lldp:nbr-list") == std::map<std::string, std::string> {
                 {"/if-name[ifName='ve-image']", ""},
@@ -34,6 +43,8 @@ TEST_CASE("Sysrepo opsdata callback")
                 {"/if-name[ifName='ve-image']/systemCapabilitiesSupported", "bridge router station-only"},
                 {"/if-name[ifName='ve-image']/systemCapabilitiesEnabled", "station-only"},
             });
+
+    spdlog::info("data");
 
     dbusServerConnection->leaveEventLoop();
 }
