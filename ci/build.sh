b0c09e45be1301395c09f3c18096944f66f1d2bd
Jan Kundrát <jan.kundrat@cesnet.cz>
2020-10-01 12:13:59 +0200

    Use patched systemd for build
    Today, systemd does not export any API for publishing the LLDP data
    obtained via systemd-networkd. Originally, we wanted to use D-Bus, but
    based on a discussion with system upstream, it was decided to publish
    the parsing code into system'd C API instead. This now means that we
    require patched systemd not just at runtime, but for the build itself as
    well.
    
    This patch configures Zuul to provide a properly patched systemd source
    tree, which should then -- hopefully -- be picked by CMake as usual.
    
    Make sure that we do not change the build env (too) much, because
    apparently this particular version of systemd cannot be built with, say,
    UBSAN just via setting the CFLAGS and CXXFLAGS and LDFLAGS -- hence the
    `env -u` dance. Also, praise to the dnf builddep plugin for not making
    me enumerate these 90 packages that get installed by hand, yay.
    
    Bug: https://github.com/systemd/systemd/pull/16744
    Change-Id: I5a787f277b1d7e2bcc0226529917906405dbca9a

--- ci/build.sh
+++ ci/build.sh
@@ -70,6 +70,16 @@ fi
 
 curl ${ARTIFACT_URL} | unzstd --stdout | tar -C ${PREFIX} -xf -
 
+# We're using a patched version of systemd which exports the LLDP parsing
+BUILD_SD=~/build-sd
+pushd ${ZUUL_PROJECT_SRC_DIR}/../../github/systemd/systemd
+env -u CC -u CXX -u LD -u CFLAGS -u CXXFLAGS -u LDFLAGS meson ${BUILD_SD}
+ninja-build -C ${BUILD_SD}
+DESTDIR=${PREFIX} ninja-build -C ${BUILD_SD} install
+export PKG_CONFIG_PATH=${PREFIX}/usr/share/pkgconfig:${PREFIX}/usr/lib64/pkgconfig:${PKG_CONFIG_PATH}
+export LD_LIBRARY_PATH=${PREFIX}/usr/lib64:${PREFIX}/usr/lib/systemd:${LD_LIBRARY_PATH}
+popd
+
 cd ${BUILD_DIR}
 cmake -GNinja -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Debug} -DCMAKE_INSTALL_PREFIX=${PREFIX} ${CMAKE_OPTIONS} ${ZUUL_PROJECT_SRC_DIR}
 ninja-build
